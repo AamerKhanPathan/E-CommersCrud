6eb39b009af87d0cc65b3cf41d8d7f971fa29220
AamerKhanPathan <aamerkhanpvt@gmail.com>
2024-06-17 18:19:56 +0530

    add loger to create user controller layer

--- src/main/java/com/bikkadit/crud_operation/controller/UserController.java
+++ src/main/java/com/bikkadit/crud_operation/controller/UserController.java
@@ -2,6 +2,9 @@ package com.bikkadit.crud_operation.controller;
 
 import com.bikkadit.crud_operation.entity.User;
 import com.bikkadit.crud_operation.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,15 +14,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@Slf4j
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private UserService service;
 
     @PostMapping("/createUser")
     public ResponseEntity<User> createUser(@RequestBody User user){
+        log.info("entering create user Controller");
 
        User save= this.service.createUser(user);
-
+log.info("user created successfully");
        return  new ResponseEntity<User>(save,HttpStatus.OK);
 
     }
